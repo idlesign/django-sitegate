f9e3e04bd7e6f8b74dc2e9018c90851592ea70c4
idle sign <idlesign@yandex.ru>
2013-05-19 15:28:35 +0700

    Test suite extended.

--- sitegate/tests.py
+++ sitegate/tests.py
@@ -1,10 +1,139 @@
 """This file contains tests for sitegate."""
 from django.utils import unittest
+from django.core import urlresolvers
+from django.shortcuts import render
+from django import template
+from django.test.client import RequestFactory
+from django.conf.urls import patterns, url
+from django.http import HttpResponseRedirect
 
+from sitegate.decorators import signin_view, signup_view, redirect_signedin, sitegate_view
 from sitegate.signup_flows.classic import *
 from sitegate.signup_flows.modern import *
 from sitegate.signin_flows.classic import *
 from sitegate.signin_flows.modern import *
+
+
+class MockUser(object):
+    def __init__(self, authorized):
+        self.authorized = authorized
+
+    def is_authenticated(self):
+        return self.authorized
+
+
+urlpatterns = patterns('',
+    url(r'entrance/', lambda r: None),
+)
+
+
+@sitegate_view(widget_attrs={'attr_name': 'attr_value', 'attr_lambda': lambda f: f.label}, template='form_bootstrap')
+def view_sitegate(request):
+    return request
+
+
+class DecoratorsTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # set urlconf to test's one
+        cls.urlconf_bak = urlresolvers.get_urlconf()
+        urlresolvers.set_urlconf('sitegate.tests')
+
+    @classmethod
+    def tearDownClass(cls):
+        urlresolvers.set_urlconf(cls.urlconf_bak)
+
+    def setUp(self):
+        self.req_factory = RequestFactory()
+
+    def test_signin(self):
+        # simple decoration
+        response = signin_view(lambda req: req)(self.req_factory.get('/signin/'))
+        self.assertTrue(hasattr(response, 'sitegate'))
+        self.assertTrue('signin_forms' in response.sitegate)
+        self.assertFalse('signup_forms' in response.sitegate)
+
+        # simple decoration + view with args
+        response = signin_view(lambda req, a1, a2='b': (req, a1, a2))(self.req_factory.get('/signin/'), 10, a2=20)
+        self.assertTrue(hasattr(response[0], 'sitegate'))
+        self.assertTrue('signin_forms' in response[0].sitegate)
+        self.assertFalse('signup_forms' in response[0].sitegate)
+        self.assertEqual(response[1], 10)
+        self.assertEqual(response[2], 20)
+
+    def test_signin(self):
+        # simple decoration
+        response = signup_view(lambda req: req)(self.req_factory.get('/signup/'))
+        self.assertTrue(hasattr(response, 'sitegate'))
+        self.assertTrue('signup_forms' in response.sitegate)
+        self.assertFalse('signin_forms' in response.sitegate)
+
+        # simple decoration + view with args
+        response = signup_view(lambda req, a1, a2='b': (req, a1, a2))(self.req_factory.get('/signup/'), 10, a2=20)
+        self.assertTrue(hasattr(response[0], 'sitegate'))
+        self.assertTrue('signup_forms' in response[0].sitegate)
+        self.assertFalse('signin_forms' in response[0].sitegate)
+        self.assertEqual(response[1], 10)
+        self.assertEqual(response[2], 20)
+
+    def test_signinup(self):
+        # simple decoration
+        response = signin_view(signup_view(lambda req: req))(self.req_factory.get('/entrance/'))
+        self.assertTrue(hasattr(response, 'sitegate'))
+        self.assertTrue('signup_forms' in response.sitegate)
+        self.assertTrue('signin_forms' in response.sitegate)
+
+        # simple decoration + view with args
+        response = signin_view(signup_view(lambda req, a1, a2='b': (req, a1, a2)))(self.req_factory.get('/entrance/'), 10, a2=20)
+        self.assertTrue(hasattr(response[0], 'sitegate'))
+        self.assertTrue('signup_forms' in response[0].sitegate)
+        self.assertTrue('signin_forms' in response[0].sitegate)
+        self.assertEqual(response[1], 10)
+        self.assertEqual(response[2], 20)
+
+    def test_redirect(self):
+        # logged in
+        # default redirect
+        request = self.req_factory.get('/entrance/')
+        request.user = MockUser(True)
+        response = redirect_signedin(lambda req: req)(request)
+        self.assertIsInstance(response, HttpResponseRedirect)
+
+        # not logged in
+        request = self.req_factory.get('/entrance/')
+        request.user = MockUser(False)
+        response = redirect_signedin(lambda req: req)(request)
+        self.assertNotIsInstance(response, HttpResponseRedirect)
+
+    def test_sitegate(self):
+        # logged in
+        request = self.req_factory.get('/entrance/')
+        request.user = MockUser(True)
+        response = sitegate_view(lambda req: req)(request)
+        self.assertIsInstance(response, HttpResponseRedirect)
+
+        # not logged in
+
+        # simple decoration
+        request = self.req_factory.get('/entrance/')
+        request.user = MockUser(False)
+        response = sitegate_view(lambda req: req)(request)
+        self.assertNotIsInstance(response, HttpResponseRedirect)
+        self.assertTrue(hasattr(response, 'sitegate'))
+        self.assertTrue('signup_forms' in response.sitegate)
+        self.assertTrue('signin_forms' in response.sitegate)
+
+        # simple decoration + view with args
+        request = self.req_factory.get('/entrance/')
+        request.user = MockUser(False)
+        response = sitegate_view(lambda req, a1, a2='b': (req, a1, a2))(request, 10, a2=20)
+        self.assertNotIsInstance(response, HttpResponseRedirect)
+        self.assertTrue(hasattr(response[0], 'sitegate'))
+        self.assertTrue('signup_forms' in response[0].sitegate)
+        self.assertTrue('signin_forms' in response[0].sitegate)
+        self.assertEqual(response[1], 10)
+        self.assertEqual(response[2], 20)
 
 
 class ClassicSignupFormsTest(unittest.TestCase):
@@ -62,7 +191,7 @@ class GenericSignupFlowTest(unittest.TestCase):
         self.assertRaises(NotImplementedError, SignupFlow)
 
     def test_getflow_name(self):
-        self.assertEquals(ModernSignup.get_flow_name(), 'ModernSignup')
+        self.assertEqual(ModernSignup.get_flow_name(), 'ModernSignup')
 
 
 class ClassicSigninFormsTest(unittest.TestCase):
