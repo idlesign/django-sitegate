cdb40abedad2d6dfff260c869cc1fa91d650cad7
Yaroslav Klyuyev <imposeren@gmail.com>
2015-08-28 02:50:50 +0300

    process_email_change is skipped if decrypted_data is not a dict instance

--- sitegate/signal_receivers.py
+++ sitegate/signal_receivers.py
@@ -3,7 +3,7 @@
 
 def process_email_change(sender, confirmation_domain, code, decrypted_data, request, *args, **kwargs):
     from sitegate.models import EmailConfirmation
-    if confirmation_domain == 'email_change':
+    if confirmation_domain == 'email_change' and isinstance(decrypted_data, dict):
         if decrypted_data.get('next_step') == 'continue_email_change':
             EmailConfirmation.continue_email_change(
                 code, decrypted_data, send_email=True, request=request
