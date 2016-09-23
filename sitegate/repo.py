c645389fd2c23fc0d475f710f2ef86ebdebe698c
idle sign <idlesign@yandex.ru>
2016-09-23 10:34:56 +0700

    Added `repo` module with known anonymous mailer hosts.

---
+++ sitegate/repo.py
@@ -0,0 +1,11 @@
+
+ANONYMOUS_MAILERS = [
+    '3a.by',
+    'dispostable.com',
+    'grandmamail.com',
+    'harakirimail.com',
+    'lackmail.ru',
+    'maildrop.cc',
+    'mailinator.com',
+]
+"""Known anonymous mailing hosts to be used in BlacklistedDomain."""
